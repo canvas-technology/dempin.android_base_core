790c5db80ba8e174df5a7da1d65ce93fa6646161
Ahmet YENEN <ahmet.yenen@misli.com>
2022-10-31 21:10:36 +0300

    [master] * Add BasicSpinnerAdapter.kt

---
+++ base_core/src/main/java/com/dempin/base_core/base/BasicSpinnerAdapter.kt
@@ -0,0 +1,21 @@
+package com.dempin.base_core.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.dempin.base_core.databinding.ItemBasicSpinnerBinding
+
+abstract class BasicSpinnerAdapter<T> :BaseSpinnerAdapter<T,ItemBasicSpinnerBinding,ItemBasicSpinnerBinding>() {
+    override fun getViewDataBinding(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup
+    ): ItemBasicSpinnerBinding {
+        return ItemBasicSpinnerBinding.inflate(layoutInflater,parent,false)
+    }
+
+    override fun getViewDataBindingDropDown(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup
+    ): ItemBasicSpinnerBinding {
+        return ItemBasicSpinnerBinding.inflate(layoutInflater,parent,false)
+    }
+}
